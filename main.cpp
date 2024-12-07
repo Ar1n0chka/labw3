ca26e174975edbc8cfde97ac7dc5802e566762ca
Arina <akorosteleva04@mail.ru>
2024-12-07 20:03:46 +0700

    1st container done&

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "lib.h"
 #include <iostream>
+#include <array>
 #include <vector>
 // 1. Создание объекта контейнера для хранения объектов типа int. 
 // 2. Добавление в контейнер десяти элементов (0, 1 … 9). 
@@ -27,22 +28,92 @@
 // • Метод/ы удаления элементов из контейнера ( erase ). 
 // • Метод получения текущего размера контейнера ( size ). 
 // • Метод/ы получения доступа по индексу ( operator[] ).
-template <typename T>
-void printContainer(const T& container) {
-    for (const auto& elem : container) {
-        std::cout << elem << " ";
+// template <typename T>
+// void printContainer(const T& container) {
+//     for (const auto& elem : container) {
+//         std::cout << elem << " ";
+//     }
+//     std::cout << std::endl;
+// }
+
+class Container
+{
+    public:
+    Container(int length = 10)
+    {
+        arr = new int [length];
     }
-    std::cout << std::endl;
-}
+
+    ~Container()
+    {
+        delete[] arr;
+    }
+    void PrintContainer()
+    {
+
+        for (int i = 0; i < capacity; ++i)
+        {
+            std::cout << arr[i] << " ";
+        }
+    }
+    void push_back(int value)
+    {
+        arr[capacity] = value;
+        capacity++; 
+    }
+    int getSize() const
+    {
+        return size;
+    }
+    int getCapacity() const
+    {
+        return capacity;
+    }
+    void erase(int index)
+    {
+        int *arrNew = new int[size - 1];
+        for(int i = 0; i < size; ++i){
+            
+            if (i < index-1)
+            { 
+                arrNew[i] = arr[i];                
+                continue;
+            }
+            if (i == index-1)
+            {
+                continue;
+            }
+            arrNew[i-1] = arr[i];
+            
+        }
+        arr = arrNew;
+        capacity--;
+    }
+    private:
+    int *arr;
+    int size = 10;
+    int capacity = 0;
+    
+
+};
+
+
 int main () 
 {
- setlocale(LC_ALL, "Ru");
- std::vector<int> vec {0, 1, 2, 3 , 4, 5, 6, 7, 8, 9, };
- std::cout << "Векторы: ";
- printContainer(vec);
+    int i;
+    std::cin >> i;
+    Container arr;
+    std::cout << "Array: ";
+    for (auto i = 0; i < 10; i++)
+    {
+        arr.push_back(i);
+    }
+    arr.PrintContainer();
+    // std::cout << arr.getSize();
+    std::cout << std::endl << "Size: " << arr.getSize() << std::endl;
+    std::cout << arr.getCapacity() << std::endl;
 
- std::cout<< sizeof(vec)<< std::endl;
-
-
+    arr.erase(i);
+    arr.PrintContainer();
 
 }
